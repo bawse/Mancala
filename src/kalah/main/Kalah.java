a05ced69fc5b2b5cc058f50dd208ae53269db0f2
Jay <jpan889@aucklanduni.ac.nz>
2017-05-08 05:13:26 +1200

    All tests pass

--- src/kalah/main/Kalah.java
+++ src/kalah/main/Kalah.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * This class is the starting point for the Modifiability Assignment.
@@ -18,12 +19,15 @@ public class Kalah {
 
 	static final int NUM_HOUSES_EACH = 6;
 	int boardLength = NUM_HOUSES_EACH * 2 + 2;
+	IO io;
+
+	int[] board = new int[]{4,4,4,4,4,4,0,4,4,4,4,4,4,0};
 
 	public static void main(String[] args) {
 		new Kalah().play(new MockIO());
 	}
 	public void play(IO io) {
-		int[] board = new int[]{4,4,4,4,4,4,0,4,4,4,4,4,4,0};
+		this.io = io;
 		// Replace what's below with your implementation
 //		io.println("+----+-------+-------+-------+-------+-------+-------+----+");
 //		io.println("| P2 | 6[ 4] | 5[ 4] | 4[ 4] | 3[ 4] | 2[ 4] | 1[ 4] |  0 |");
@@ -33,45 +37,133 @@ public class Kalah {
 //		io.println("Player 1's turn - Specify house number or 'q' to quit: ");
 		int playerTurn = 1;
 		//int indexMultiplier = 1; //player 1 always goes first
-		String input = io.readFromKeyboard("Player 1's turn - Specify house number or 'q' to quit: "); // add min/max bounds 1-6
-		int houseNumber = Integer.parseInt(input);
+		int houseNumber;
 
-		//System.out.println(Arrays.toString(board));
-		playerTurn = performMove(board, playerTurn, houseNumber);
-		//System.out.println(Arrays.toString(board));
-		//System.out.println(playerTurn);
+		boardString(board, io, playerTurn);
+		while (!gameOver(board, playerTurn) && (houseNumber = io.readInteger("Player P"+playerTurn+"'s turn - Specify house number or 'q' to quit: ",1,6,-1,"q")) >= 0) {
 
-		//System.out.println(seeds);
-//		for (int i=houseNumber+1;seeds>0;i++){
-//			if (i > player1.length - 1){
-//				int remainingSeeds = seeds;
-//				for (int j=0;remainingSeeds>0;remainingSeeds--,j++){
-//					player2[j]++;
-//				}
-//				break;
-//			}
-//			player1[i]++;
-//			seeds--;
-//		}
-//
-//		List<Integer> player2Board = toList(player2);
-//		Collections.reverse(player2Board);
-//		System.out.println(Arrays.toString(player2Board.toArray()));
-//		System.out.println(Arrays.toString(player1));
+			playerTurn = performMove(board, playerTurn, houseNumber);
+			boardString(board, io, playerTurn);
+		}
+		//game over message
+		io.println("Game over");
+		boardString(board,io,playerTurn);
+		if (gameOver(board, playerTurn)) {
+			displayScore(board);
+		}
 
+	}
+
+	public void displayScore(int[] board){
+
+		int[] p1 = Arrays.copyOfRange(board, 0, board.length/2);
+		int[] p2 = Arrays.copyOfRange(board, board.length/2, board.length);
+
+		int player1Score = IntStream.of(p1).sum();
+		int player2Score = IntStream.of(p2).sum();
+
+		io.println("\t" + "player 1:" + player1Score);
+		io.println("\t" + "player 2:" + player2Score);
+		if (player1Score > player2Score){
+			io.println("Player 1 wins!");
+		} else if (player2Score > player1Score){
+			io.println("Player 2 wins!");
+		} else {
+			io.println("A tie!");
+		}
+	}
+
+	public boolean gameOver(int[] board, int playerTurn){
+		int[] p1 = Arrays.copyOfRange(board, 0, board.length/2 -1);
+		int[] p2 = Arrays.copyOfRange(board, board.length/2, board.length -1 );
+
+		boolean gameOver = true;
+		if (playerTurn == 1){
+			for (int seeds : p1){
+				if (seeds != 0){
+					return false;
+				}
+			}
+		} else {
+			for (int seeds : p2){
+				if (seeds != 0){
+					return false;
+				}
+			}
+		}
+		return gameOver;
+
+	}
+
+	public void setBoard(int[] newBoard){
+		this.board = newBoard;
+	}
+
+	public void boardString(int[] board, IO io, int playerTurn){
+		StringBuilder stringBuilder = new StringBuilder();
+		int[] p1 = Arrays.copyOfRange(board, 0, board.length/2);
+		int[] p2 = Arrays.copyOfRange(board, board.length/2, board.length);
+		int p1Store = p1[p1.length - 1];
+		int p2Store = p2[p2.length - 1];
+		Collections.reverse(Ints.asList(p2));
+		io.println("+----+-------+-------+-------+-------+-------+-------+----+");
+
+		for (int i=0; i<p2.length + 1;i++){
+			if (i == 0){
+				stringBuilder.append("| P2 | ");
+			} else if (i == (p2.length)){
+				stringBuilder.append(seedString(p1Store) + " |");
+			} else {
+				stringBuilder.append(p2.length - i); // house number
+				stringBuilder.append("[" + seedString(p2[i]) + "] | ");
+			}
+		}
+		io.println(stringBuilder.toString());
+		stringBuilder.setLength(0);
+		io.println("|    |-------+-------+-------+-------+-------+-------|    |");
+
+		for (int i=0;i<p1.length + 1;i++){
+			if (i==0){
+				stringBuilder.append("| "+seedString(p2Store)+" | ");
+			} else if (i == p2.length){
+				stringBuilder.append("P1 |");
+			} else {
+				stringBuilder.append(i); // house number
+				stringBuilder.append("[" + seedString(p1[i-1]) + "] | ");
+			}
+		}
+		io.println(stringBuilder.toString());
+		io.println("+----+-------+-------+-------+-------+-------+-------+----+");
+	}
+
+
+	public String seedString(int seeds){
+		if (seeds >=10){
+			return "" + seeds;
+		} else {
+			return " " + seeds;
+		}
 	}
 
 	public int performMove(int[] board, int playerTurn, int houseNumber){ // returns a value that indicates which players turn it is after performing the move
 
-		//TODO: THIS ONLY WORKS FOR PLAYER1 TURN - NEED TO CALCULATE HOUSEINDEX IF PLAYERTURN=2
+
 		int seeds;
 		int startIndex;
 		if (playerTurn == 1){
 			seeds = board[houseNumber -1];
+			if (seeds == 0){
+				io.println("House is empty. Move again.");
+				return playerTurn;
+			}
 			board[houseNumber - 1]=0;
 			startIndex = houseNumber;
 		} else {
 			seeds = board[houseNumber + 6];
+			if (seeds == 0){
+				io.println("House is empty. Move again.");
+				return playerTurn;
+			}
 			board[houseNumber + 6] = 0;
 			startIndex = houseNumber + 7; // change hard-coded numbers to use constants during refactor
 		}
@@ -89,16 +181,19 @@ public class Kalah {
 			}
 		}
 		if (outcome1(finalIndex, playerTurn, board)){
-			System.out.println("outcome1");
+
+			//System.out.println("outcome1");
 			if (playerTurn == 1){
-		        return 2;
+				return 2;
             } else { return 1; }
         } else if (outcome2(finalIndex, playerTurn, board)){
-			System.out.println("outcome2");
+			//System.out.println("outcome2");
 			return playerTurn; // player gets another turn
 		} else if (outcome3(finalIndex, playerTurn, board)){
-			System.out.println("outcome3");
-			capture(finalIndex, playerTurn, board);
+			board = capture(finalIndex, playerTurn, board);
+			//System.out.println("outcome3");
+			//System.out.println(Arrays.toString(board));
+			setBoard(board);
 			if (playerTurn == 1) return 2; else return 1;
 		} else {
 			if (playerTurn == 1) return 2; else return 1;
@@ -150,7 +245,7 @@ public class Kalah {
 		int houseNo;
 		boolean ownHouse;
 
-		if (finalIndex < (board.length/2) -1){
+		if (finalIndex <= (board.length/2) -1){
 			houseNo = finalIndex + 1;
 			if (playerTurn == 1){ // if finalIndex is on left-hand side of board and its P1's turn, then they own the house
 				ownHouse = true;
@@ -197,7 +292,7 @@ public class Kalah {
 		}
 	}
 
-	public void capture (int finalIndex, int playerTurn, int[] board){
+	public int[] capture (int finalIndex, int playerTurn, int[] board){
 
 		int player1StoreIndex = (boardLength/2) - 1;
 		int player2StoreIndex = boardLength - 1;
@@ -227,7 +322,6 @@ public class Kalah {
 
 		if (playerTurn == 1){
 			seedsToTransfer = seedsToTransfer + rearrangedBoard.get((houseNo - 1) + rearrangedBoard.size()/2);
-			System.out.println((houseNo - 1) + rearrangedBoard.size()/2);
 			rearrangedBoard.set((houseNo - 1) + (rearrangedBoard.size()/2), 0);
 			rearrangedBoard.add(rearrangedBoard.size()/2, player1Store + seedsToTransfer);
 			rearrangedBoard.add(rearrangedBoard.size()/2 + 1, player2Store);
@@ -239,51 +333,40 @@ public class Kalah {
 
 		}
 		int[] rearrangedBoardArray = Ints.toArray(rearrangedBoard);
-		System.out.println(Arrays.toString(rearrangedBoardArray));
+		//System.out.println(Arrays.toString(rearrangedBoardArray));
 
-//		if (finalIndex > NUM_HOUSES_EACH){
-//			seedsToTransfer =+ board[finalIndex - (boardLength/2)];
-//			board[finalIndex - (boardLength/2)] = 0;
-//		} else {
-//			seedsToTransfer =+ board[finalIndex + (boardLength/2)];
-//			board[finalIndex + (boardLength/2)] = 0;
-//		}
-//
-//		if (playerTurn == 1){
-//			board[(boardLength/2) - 1] =+ seedsToTransfer; // P1 Store
-//		} else {
-//			board[boardLength - 1] =+ seedsToTransfer; // P2 Store
-//		}
+		int[] p1 = Arrays.copyOfRange(rearrangedBoardArray, 0, rearrangedBoardArray.length/2);
+		int[] p2 = Arrays.copyOfRange(rearrangedBoardArray, rearrangedBoardArray.length/2, rearrangedBoardArray.length);
+		Collections.reverse(Ints.asList(p2));
+		int[] newBoard = Ints.concat(p1,p2);
+
+		return newBoard;
 	}
 
 	public boolean oppositeHouseNotEmpty(int finalIndex, int playerTurn, int[] board){
 
 		int houseNo;
 		if (finalIndex > (boardLength/2 - 1)){
-			houseNo = finalIndex - boardLength/2;
+			houseNo = finalIndex - boardLength/2 + 1;
 		} else {
-			houseNo = finalIndex - 1;
+			houseNo = finalIndex + 1;
 		}
+
 		int[] clone = board.clone();
-		int[] player2Side = Arrays.copyOfRange(clone,boardLength/2, boardLength - 2); // player2 without store
-		int[] player1Side = Arrays.copyOfRange(clone, 0, boardLength/2 - 2); // player1 without store
+		int[] player2Side = Arrays.copyOfRange(clone,boardLength/2, boardLength - 1); // player2 without store
+		int[] player1Side = Arrays.copyOfRange(clone, 0, boardLength/2 - 1); // player1 without store
 
 		List<Integer> player2List = toList(player2Side);
 		List<Integer> player1List = toList(player1Side);
 		Collections.reverse(player2List);
 
 		if (playerTurn == 1){
+			//System.out.println(houseNo - 1); // 6
 			return (player2List.get((houseNo - 1)) > 0);
 		} else {
 			return (player1List.get(player1List.size() - houseNo) > 0);
 		}
-//
-//
-//		if (finalIndex > NUM_HOUSES_EACH) {
-//			return (board[finalIndex - (boardLength/2)] > 0);
-//		} else {
-//			return (board[finalIndex + (boardLength/2)] > 0);
-//		}
+
 
 	}
 	public List<Integer> toList(int[] intArray){
